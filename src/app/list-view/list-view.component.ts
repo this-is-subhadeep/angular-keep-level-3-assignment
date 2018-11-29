30ed1c2c308dfc9513bf6f9bc3016f23e411945d
Mitra <201538@cognizant.com>
2018-11-29 19:25:00 +0530

    using array.filter

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from '../note';
+import { NotesService } from '../services/notes.service';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.css']
 })
-export class ListViewComponent {
+export class ListViewComponent implements OnInit {
 
   notStartedNotes: Array<Note>;
   startedNotes: Array<Note>;
   completedNotes: Array<Note>;
+  constructor(private notesService: NotesService) { }
+
+  ngOnInit() {
+    this.notesService.getNotes().subscribe(notes => {
+      this.notStartedNotes = notes.filter(note => note.state === 'not-started');
+      this.startedNotes = notes.filter(note => note.state === 'started');
+      this.completedNotes = notes.filter(note => note.state === 'completed');
+    });
+  }
 }
